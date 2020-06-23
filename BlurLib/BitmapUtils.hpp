9cfa46df1ca5ff3ef9ed819d8ca4a5fc766aacaf
Nick Yegorov <eligant.ru@gmail.com>
2020-06-23 04:19:54 +0300

    pragma once

--- BlurLib/BitmapUtils.hpp
+++ BlurLib/BitmapUtils.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #pragma warning(disable : 4714)
 #pragma warning(disable : 4244)
 
--- BlurLib/ThreadUtils.hpp
+++ BlurLib/ThreadUtils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#pragma once
+
 #include <thread>
 
 enum class ThreadPriority : size_t
